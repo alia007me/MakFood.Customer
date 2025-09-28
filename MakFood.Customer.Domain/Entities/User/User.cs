469a7be010aaa35792c88c7cfb3bdd75e8dfbe42
Your Name <your.email@example.com>
2025-09-28 19:01:57 +0330

    تکمیل مدل یوزر

--- MakFood.Customer.Domain/Entities/User/User.cs
+++ MakFood.Customer.Domain/Entities/User/User.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace MakFood.Customer.Domain.Models.Entities.User
 {
-    internal class User
+    public class UserAccount
     {
+        public UserAccount(IdentityInformation identity, AccountInformation account, ContactInformation contactinfo, List<Address> addresses)
+        {
+            id = Guid.NewGuid();
+
+            this.identity = identity;
+            this.account = account;
+            this.contactinfo = contactinfo;
+            this.addresses = addresses;
+        }
+
+        public Guid id { get; set; }
+        public IdentityInformation identity { get; set; }
+        public AccountInformation account { get; set; }
+        public ContactInformation contactinfo { get; set; }
+        public List<Address> addresses { get; set; }
+
+
     }
 }
