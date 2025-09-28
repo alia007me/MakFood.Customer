21e298289d0a4fd605f111b2e6323d3fc878d687
Your Name <your.email@example.com>
2025-09-28 17:35:21 +0330

    اضافه کردن فولدر ها ی اصلی بر مبنای معماری تمیز

---
+++ MakFood.Customer.Domian.Entities/Entity/User/ContactInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Customer.Domian.Entities.Entity.User
+{
+    internal class ContactInformation
+    {
+    }
+}
