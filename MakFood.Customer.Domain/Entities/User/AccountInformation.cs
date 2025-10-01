0ca571b7fb8ca9b3904ffe064f64de529cc0c3f4
Your Name <your.email@example.com>
2025-10-01 10:32:57 +0330

    رمز در مدل Customer مدیریت نمیشود، مربوط به Authentication است
    عدم تطابق با مدل تایید شده
    انجام شد

--- MakFood.Customer.Domain/Entities/User/AccountInformation.cs
+++ MakFood.Customer.Domain/Entities/User/AccountInformation.cs
@@ -5,9 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using MakFood.Customer.Domain.Helper;
-using MakFood.Customer.Domain.Helper.HashHelper;
-
 namespace MakFood.Customer.Domain.Models.Entities.User
 {
     /// <summary>
@@ -26,30 +23,15 @@ namespace MakFood.Customer.Domain.Models.Entities.User
         /// <remarks>
         /// با توجه به منطق پروژه در این کانستراکتور مقدار ورودی گرفتن لازم نبود
         /// </remarks>
-        public AccountInformation(string userName, string password, string reWritePassword)
+        public AccountInformation()
         {
 
-            ValidityUserName(userName);
-            ValidityPassword(password);
-            CheckPasswordRewrite(password, reWritePassword);
-
             Id = Guid.NewGuid();
-            this.UserName = userName;
-            this.Password = password;
-
             JoinDate = DateOnly.FromDateTime(DateTime.Now);
             Badge = Badge.Normal;
         }
 
         public Guid Id { get; private init; }
-        public string UserName { get; private set; }
-        private string Password
-        {
-            set
-            {
-                _password = HashHelper.ComputeSha256Hash(value);
-            }
-        }
         public DateOnly JoinDate { get; private set; }
         public string? ProfilePicture { get;private set; }
         public Badge Badge { get; private set; }
@@ -68,66 +50,6 @@ namespace MakFood.Customer.Domain.Models.Entities.User
         }
 
         /// <summary>
-        /// پسوورد رو برسی می کنه
-        /// </summary>
-        /// <param name="password"></param>
-        /// <exception cref="Exception"></exception>
-        /// <remarks>فقط بیشتر از 6 کاراکتر باشه کافیه</remarks>
-        public void ValidityPassword(string password)
-        {
-            if (password == null) throw new Exception("Your password can't be Null");
-            string passwordRegex = "^.{6,}$\r\n";
-            if (!Regex.IsMatch(password, passwordRegex)) throw new Exception("at list 6 character");
-        }
-
-        /// <summary>
-        /// یکسان بودن پسورد با تکرارش رو برسی می کنه
-        /// </summary>
-        /// <param name="password"></param>
-        /// <param name="rewritepassword"></param>
-        /// <exception cref="Exception"></exception>
-        public void CheckPasswordRewrite(string password, string rewritepassword)
-        {
-            if (password != rewritepassword) throw new Exception("password and replay of it are not same");
-        }
-
-        /// <summary>
-        /// یوزر نیم را آپدیت می کند
-        /// </summary>
-        /// <param name="userName"></param>
-        public void UpdateUserName(string userName)
-        {
-            ValidityUserName(userName);
-            UserName = userName;
-        }
-
-        /// <summary>
-        /// پسورد یوزر رو بعد از برسی شروط آپدیت می کنه
-        /// </summary>
-        /// <param name="Previouspassword">رمز قبلی</param>
-        /// <param name="newPassword">رمز جدید</param>
-        /// <exception cref="Exception"></exception>
-        public void UpdateUserPassword(string Previouspassword, string newPassword)
-        {
-            if (CheckPassword(Previouspassword)) throw new Exception("your Previous password is wrong");
-            ValidityPassword(newPassword);
-
-            Password = newPassword;
-
-        }
-
-        /// <summary>
-        /// برسی می کنه هش شده پسوردی که وارد کرده با هش ذخیره شده یکسان هست یا نه
-        /// </summary>
-        /// <param name="password"></param>
-        /// <returns></returns>
-        public bool CheckPassword(string? password)
-        {
-            return HashHelper.VerifySha256Hash(password, _password);
-        }
-
-
-        /// <summary>
         /// این متد عکس پروفایل جدید برای کاربر ثبت می کند
         /// </summary>
         /// <param name="url"></param>
@@ -138,6 +60,7 @@ namespace MakFood.Customer.Domain.Models.Entities.User
         {
             ProfilePicture = url;
         }
+
 
         /// <summary>
         ///  این متد عکس پروفایل را به صورت کامل حذف می کند و عکس پروفایل خالی می ماند
