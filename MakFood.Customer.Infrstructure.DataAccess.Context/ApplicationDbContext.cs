69b4779b6b4474356d55ba3bf85c73c157747560
Your Name <your.email@example.com>
2025-10-01 15:00:20 +0330

    جابجایی اینترفیس ها برای بار چندم از اپلیکیشن به دامین

---
+++ MakFood.Customer.Infrstructure.DataAccess.Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+using MakFood.Customer.Domain.Models.Entities;
+using MakFood.Customer.Domain.Models.Entities.User;
+
+namespace MakFood.Customer.Infrstructure.DataAccess.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>();
+                
+        }
+    }
+}
