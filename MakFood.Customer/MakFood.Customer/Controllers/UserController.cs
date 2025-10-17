0de5ccfb80bbddaed0e0f83d03219983091fa189
Your Name <your.email@example.com>
2025-10-17 21:26:46 +0330

    fix توی تصحیح کانفیلیت ها مشکل ایجاد شده بود

--- MakFood.Customer/MakFood.Customer/Controllers/UserController.cs
+++ MakFood.Customer/MakFood.Customer/Controllers/UserController.cs
@@ -1,7 +1,7 @@
 
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using MakFood.Customer.Application.Commands.ProfileThumbnail;
-﻿using MakFood.Customer.Application.Commands.User.AddUserAddress;
+using MakFood.Customer.Application.Commands.User.AddUserAddress;
 using MakFood.Customer.Application.Commands.User.RegisterUser;
 using MakFood.Customer.Application.Commands.User.RemoveUserAddress;
 using MakFood.Customer.Application.Commands.User.UpdateUserAddress;
@@ -25,7 +25,7 @@ public class UserController : ControllerBase
     [HttpPatch("{userId}/Profile/Thumbnail")]
     public async Task<IActionResult> SetOrUpdate([FromBody] SetOrUpdateProfileThumbnailCommand command)
     {
-        
+
         var response = await _mediator.Send(command);
         return Ok(response);
     }
@@ -37,70 +37,64 @@ public class UserController : ControllerBase
         var command = new RemoveProfileThumbnailCommand
         {
             UserId = userId,
-            
+
         };
 
         var response = await _mediator.Send(command);
         return Ok(response);
+    }
+    [HttpPost]
+    public async Task<IActionResult> RegisterUser([FromBody] RegisterUserCommand command, CancellationToken ct)
+    {
+        if (command == null)
+            return BadRequest("empty or wrong input");
 
-        [HttpPost]
-        public async Task<IActionResult> RegisterUser([FromBody] RegisterUserCommand command, CancellationToken ct)
-        {
-            if (command == null)
-                return BadRequest("empty or wrong input");
+        var target = await _mediator.Send(command, ct);
 
-            var target = await _mediator.Send(command, ct);
+        return Ok(target);
+    }
 
-            return Ok(target);
-        }
+    [HttpPost("{id}/Address")]
+    public async Task<IActionResult> AddUserAddress([FromBody] AddUserAddressCommand command, CancellationToken ct)
+    {
+        if (command == null)
+            return BadRequest("empty or wrong input");
 
-        [HttpPost("{id}/Address")]
-        public async Task<IActionResult> AddUserAddress([FromBody] AddUserAddressCommand command, CancellationToken ct)
-        {
-            if (command == null)
-                return BadRequest("empty or wrong input");
+        var target = await _mediator.Send(command, ct);
 
-            var target = await _mediator.Send(command, ct);
+        return Ok(target);
+    }
 
-            return Ok(target);
-        }
+    [HttpPut("{id}/Address/{Addresid}")]
+    public async Task<IActionResult> UpdateUserAddress([FromBody] UpdateUserAddressCommand command, CancellationToken ct)
+    {
+        if (command == null)
+            return BadRequest("empty or wrong input");
 
-        [HttpPut("{id}/Address/{Addresid}")]
-        public async Task<IActionResult> UpdateUserAddress([FromBody] UpdateUserAddressCommand command, CancellationToken ct)
-        {
-            if (command == null)
-                return BadRequest("empty or wrong input");
+        var target = await _mediator.Send(command, ct);
 
-            var target = await _mediator.Send(command, ct);
+        return Ok(target);
+    }
 
-            return Ok(target);
-        }
+    [HttpDelete("{id}/Address/{Addresid}")]
+    public async Task<IActionResult> RemoveUserAddress([FromBody] RemoveUserAddressCommand command, CancellationToken ct)
+    {
+        if (command == null)
+            return BadRequest("empty or wrong input");
 
-        [HttpDelete("{id}/Address/{Addresid}")]
-        public async Task<IActionResult> RemoveUserAddress([FromBody] RemoveUserAddressCommand command, CancellationToken ct)
-        {
-            if (command == null)
-                return BadRequest("empty or wrong input");
+        var target = await _mediator.Send(command, ct);
 
-            var target = await _mediator.Send(command, ct);
+        return Ok(target);
+    }
 
-            return Ok(target);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateIdentityInformation([FromBody] UpdateUserInformationCommand command, CancellationToken ct)
+    {
+        if (command == null)
+            return BadRequest("empty or wrong input");
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateIdentityInformation([FromBody] UpdateUserInformationCommand command, CancellationToken ct)
-        {
-            if (command == null)
-                return BadRequest("empty or wrong input");
+        var target = await _mediator.Send(command, ct);
 
-            var target = await _mediator.Send(command, ct);
-
-            return Ok(target);
-        }
-
-
-
-
-
+        return Ok(target);
     }
 }
